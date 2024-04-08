24cb5d681ecbd59bd5eb7d4f814e60f0afca2917
schwarzn <n.schwarz@wksgruppe.de>
2024-04-08 10:31:23 +0200

    Init

--- test/led_onboard.py
+++ test/led_onboard.py
@@ -5,11 +5,12 @@ from time import sleep
 # Initialisierung von GPIO25 als Ausgang
 led_onboard = Pin(25, Pin.OUT)
 
-# LED einschalten
-led_onboard.on()
+while(1):
+    # LED einschalten
+    led_onboard.on()
+    # 5 Sekunden warten
+    sleep(0.5)
+    # LED ausschalten
+    led_onboard.off()
+    sleep(0.5)
 
-# 5 Sekunden warten
-sleep(1)
-
-# LED ausschalten
-led_onboard.off()
